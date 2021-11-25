fa5c44cad375cbbe5768e8d668d8b2276a750b89
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-11-25 23:35:11 +0100

    Added views tests

--- store/test_views.py
+++ store/test_views.py
@@ -1,11 +1,40 @@
 from unittest import skip
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from store.models import Category, Product, User
+from django.http import HttpRequest
+from store.views import all_products
 
 
-@skip("demonstrating skipping")
-class TestSkip(TestCase):
-    def test_skip_example(self):
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        Category.objects.create(name='DVD', slug='DVD')
+        User.objects.create(username='admin')
+        Product.objects.create(category_id=1, title='Orange', created_by_id=1, slug='Orange',
+                               price='5.00', image='orange')
+
+    def test_url_allowed_hosts(self):
         """
-        Demonstrating skipping a test
+        Test allowed hosts
         """
-        pass
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        response = self.c.get(reverse('store:product_detail', args=['Orange']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_url(self):
+        response = self.c.get(reverse('store:category_list', args=['DVD']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        print(html)
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
