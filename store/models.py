2d06ac15dd1bec8035cc015ca3147836f2f9073c
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-12-01 13:03:51 +0100

    Minor refactoring

--- store/models.py
+++ store/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     created_by = models.ForeignKey(User, related_name='product_creator', on_delete=models.CASCADE, default='admin')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    image = models. ImageField(upload_to='images/', blank=True)
+    image = models. ImageField(upload_to='images/', default='images/default.jpg')
     slug = models.SlugField(max_length=200)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
--- store/views.py
+++ store/views.py
@@ -5,14 +5,14 @@ from .models import Category, Product
 # Create your views here.
 
 
-def all_products(request):
+def product_all(request):
     products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
 
 
 def category_list(request, category_slug):
