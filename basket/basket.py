64f62f1899abc594b95f051020919be1d0419439
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-12-04 17:28:25 +0100

    url & view for the add function and updating the basket class

--- basket/basket.py
+++ basket/basket.py
@@ -11,3 +11,12 @@ class MyBasket:
         if 'session_key' not in request.session:
             basket = self.session['session_key'] = {}
         self.basket = basket
+
+    def add(self, product):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price)}
+        self.session.modified = True
