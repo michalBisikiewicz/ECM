5f86adfd417899ced0b49287190c0b7efa190a71
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-12-05 14:40:25 +0100

    Added quantity to the session data

--- basket/basket.py
+++ basket/basket.py
@@ -12,11 +12,17 @@ class MyBasket:
             basket = self.session['session_key'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         Adding and updating the users basket session data
         """
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
--- basket/views.py
+++ basket/views.py
@@ -17,7 +17,9 @@ def basket_add(request):
     basket = MyBasket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
-        response = JsonResponse({'test': 'data'})
+        basket.add(product=product, qty=product_qty)
+        basket_qty = basket.__len__()
+        response = JsonResponse({'qty': basket_qty})
         return response
