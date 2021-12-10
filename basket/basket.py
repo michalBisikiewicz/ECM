eb27ca6102da3a01e551dc2b2bd8b6154638904a
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-12-10 15:53:27 +0100

    Fixed issue with adding products to the basket

--- basket/basket.py
+++ basket/basket.py
@@ -19,8 +19,10 @@ class MyBasket:
         """
         Adding and updating the users basket session data
         """
-        product_id = product.id
-        if product_id not in self.basket:
+        product_id = str(product.id)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
             self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
         self.save()
 
