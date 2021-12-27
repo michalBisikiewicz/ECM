af26c386e919aaffd8a504a0a5a061e2d0d07031
michalBisikiewicz <bisikiewiczmichal@gmail.com>
2021-12-27 11:52:47 +0100

    Built form & view for user sign up

---
+++ account/views.py
@@ -0,0 +1,23 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from .forms import RegistrationForm
+# Create your views here.
+from .token import account_activation_token
+
+
+def account_register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
